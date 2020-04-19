ab3fad90532a8500721ef0cbc63a20240c8f0f05
gj2528 <44559078+gj2528@users.noreply.github.com>
2020-04-19 10:52:27 +0800

    Add files via upload

--- get_edge.py
+++ get_edge.py
@@ -37,7 +37,7 @@ for i in range(int(0.9 * num)):
     print(edge_img.shape,'edge.shape')
     rain_img_raw = rain_img.astype(np.uint8)
     print(rain_img_raw,'rain_img_raw')
-    rain_concat = tf.concat([rain_img_raw,edge_img],2)
+    rain_concat = np.concatenate([rain_img_raw,edge_img], 2)
     img_raw = rain_concat.tobytes()
 
     clean_img = plt.imread(deep_joint_rain_train_data + 'clean_image/' + clean_name) * 255.0  ## path of gt img
@@ -66,7 +66,7 @@ for i in range(int(0.9 * num), num):
     rain_img = plt.imread(deep_joint_rain_train_data + 'rain_image/' + im_name) * 255.0
     edge_img = edge_compute(rain_img)
     rain_img_raw = rain_img.astype(np.uint8)
-    rain_concat = tf.concat([rain_img_raw, edge_img], 2)
+    rain_concat = np.concatenate([rain_img_raw, edge_img], 2)
     img_raw = rain_concat.tobytes()
 
     clean_img = plt.imread(deep_joint_rain_train_data + 'clean_image/' + clean_name) * 255.0  ## path of gt img
